55e007d946631685bc457512c354d9fbdf1e86cd
Jonathan Conway <jonathan@jaikoo.com>
2016-02-25 14:54:42 +0000

    Use expect syntax
    Changed the test so use the 'expect' syntax rather than 'should'

--- tests/homepage_test.js
+++ tests/homepage_test.js
@@ -1,19 +1,11 @@
 var request = require('request');
 var url = 'http://localhost';
+var expect = require('chai').expect;
 
 describe('Homepage', function () {
-
-    before(function (done) {
-
-    });
-
-    after(function (done) {
-
-    });
-
     it('should get root ok', function (done) {
         request(url, function (error, response, body) {
-            response.statusCode.should.eql(200);
+            expect(response.statusCode).to.equal(200);
             // body.should.eql({ data: 'Some data'});
             done();
         });
